1a7426da3d4f4b13053f4e5d57a7368fec6b9f56
unknown <francoseguellucero@gmail.com>
2019-06-16 17:35:24 -0400

    Se hace el cuaderno con los graficos de *10.csv

--- grafico10MX.py
+++ grafico10MX.py
@@ -3,14 +3,40 @@ import matplotlib.pyplot as plt
 nombreArchivo="2019-06-13_16_48_21_mtq1_duty_10.csv"
 archivo=open(nombreArchivo,"r")
 mx=[]
+my=[]
+mz=[]
+
+gx=[]
+gy=[]
+gz=[]
+
 time=[]
+
+tail1=[]
+tail2=[]
+tail3=[]
+
+checksum=[]
 stringDatos=archivo.readline()
 while(stringDatos!=""):
     dataStr=stringDatos.split(",")
-    #obtener el mx y agregarlo al arreglo mx
+    #obtener el m y agregarlo al arreglo mx
     mx.append(float(dataStr[0]))
+    my.append(float(dataStr[1]))
+    mz.append(float(dataStr[2]))
+    
+    #obtener el g y agregarlo al arreglo my
+    gx.append(float(dataStr[3]))
+    gy.append(float(dataStr[4]))
+    gz.append(float(dataStr[5]))
+
     #obtener el time y agregarlo al arreglo time
     time.append(float(dataStr[6]))
+    
+    #obtener el tail y agregarlo a su arreglo
+    tail1.append(float(dataStr[7]))
+    tail2.append(float(dataStr[8]))
+    tail3.append(float(dataStr[9]))
     stringDatos=archivo.readline()
 archivo.close()
 
@@ -19,6 +45,5 @@ plt.xlabel("Tiempo")
 plt.ylabel("mx")
 plt.title("Eje x de M en función del tiempo")
 plt.show()
-
 
 
